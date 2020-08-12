8c27d4d9266cdfb97c51d9f5eccc445b6379716e
Paulo Henrique <phenriq694@gmail.com>
2020-08-12 01:30:50 -0300

    Test 'should be able to list the transactions' solved

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -30,12 +30,21 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
   // const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get('transactions');
+
+      const transactionsFormatted = response.data.transactions.map(
+        (transaction: Transaction) => ({
+          ...transaction,
+          formattedValue: formatValue(transaction.value),
+        }),
+      );
+
+      setTransactions(transactionsFormatted);
     }
 
     loadTransactions();
@@ -81,18 +90,19 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map((transaction: Transaction) => {
+                return (
+                  <tr key={transaction.id}>
+                    <td className="title">{transaction.title}</td>
+                    <td className={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {transaction.formattedValue}
+                    </td>
+                    <td>{transaction.category.title}</td>
+                    <td>{transaction.created_at}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </TableContainer>
